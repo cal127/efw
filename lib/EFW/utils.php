256dd16a76297a9939fb732ccd67c3d7eb0c54f3
Ege <caliph_wq@yahoo.co.uk>
2013-03-02 19:32:27 +0200

    Bugfix

--- lib/EFW/utils.php
+++ lib/EFW/utils.php
@@ -31,7 +31,7 @@ function _url($qs) {
     $u = EFW::$conf['url'];
     $p = EFW::$mods_conf['Tpl']['options']['pretty_urls'];
 
-    if (!preg_match('/^.*\.(?:css|js|png|jpg|jpeg|gif)$/', $qs) || $p) {
+    if (preg_match('/^.*\.(?:css|js|png|jpg|jpeg|gif)$/', $qs) || $p) {
         return $u . '/' . $qs;
     }
 
