af636f6ff0eb1d5b7a91cdcd3b1e53091ce654a3
Ege <caliph_wq@yahoo.co.uk>
2013-05-06 17:28:42 +0300

    Added .swf extension to _url function. This _url function needs to be fixed immediately

--- src/utils.php
+++ src/utils.php
@@ -6,7 +6,7 @@ function _url($qs) {
     $u = EFW::$conf['url'];
     $p = EFW::$conf['pretty_urls'];
 
-    if (preg_match('/^.*\.(?:css|js|png|jpg|jpeg|gif)$/', $qs) || $p) {
+    if (preg_match('/^.*\.(?:css|js|png|jpg|jpeg|gif|swf)$/', $qs) || $p) {
         return $u . '/' . $qs;
     }
 
