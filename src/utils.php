eb6d8ab4393d1d4c6a353109de48e7d650a2ac51
Ege <caliph_wq@yahoo.co.uk>
2013-05-06 17:53:04 +0300

    added .mp3 to _url. oh la la this function

--- src/utils.php
+++ src/utils.php
@@ -6,7 +6,7 @@ function _url($qs) {
     $u = EFW::$conf['url'];
     $p = EFW::$conf['pretty_urls'];
 
-    if (preg_match('/^.*\.(?:css|js|png|jpg|jpeg|gif|swf)$/', $qs) || $p) {
+    if (preg_match('/^.*\.(?:css|js|png|jpg|jpeg|gif|swf|mp3)$/', $qs) || $p) {
         return $u . '/' . $qs;
     }
 
