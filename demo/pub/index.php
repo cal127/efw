01387f112275395071110a9258d50a2fbff40fac
Ege Avunç <egeavunc@gmail.com>
2013-12-01 12:45:25 +0200

    Small enhancement on index + change README

--- demo/pub/index.php
+++ demo/pub/index.php
@@ -1,5 +1,15 @@
 <?php
 
-require_once '../vendor/autoload.php';
+$autoload_file = __DIR__ . '../vendor/autoload.php';
+
+if (!is_file($autoload_file)) {
+    throw new Exception(
+          'Either vendor folder or autoload.php is missing.'
+        . ' Make sure you\'ve executed "composer install" command before'
+        . ' testing your application.'
+    );
+}
+
+require_once $autoload_file;
 
 EFW\EFW::boot();
